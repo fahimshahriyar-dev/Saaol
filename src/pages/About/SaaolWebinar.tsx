937d9478d2a6ef935dcccc878b63b58e63c2be8a
Fahim Shahriyar Mugdho <fahimshahriyar.dev@gmail.com>
2026-07-04 10:46:57 +0600

    Update website

--- src/pages/About/SaaolWebinar.tsx
+++ src/pages/About/SaaolWebinar.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const SaaolWebinar = () => {
     return (
         <div>SaaolWebinar</div>
--- src/pages/Treatments/TreatmentsOverview.tsx
+++ src/pages/Treatments/TreatmentsOverview.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const TreatmentsOverview = () => {
     return (
         <div>TreatmentsOverview</div>
